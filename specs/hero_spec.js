55c6e5a6db6e363818f59e316e3b89adebae2ad4
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-17 23:22:53 +0000

    Added favouriteFood property to Hero with test

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -6,7 +6,7 @@ describe("Hero Tests", function(){
   let hero1;
 
   beforeEach("Setup", function(){
-    hero1 = new Hero("Sir Lancelot the Brave", 100);
+    hero1 = new Hero("Sir Lancelot the Brave", 100, "Rabbit");
   });
 
   it("should have a name", function(){
@@ -15,6 +15,10 @@ describe("Hero Tests", function(){
 
   it("should have health", function(){
     assert.strictEqual(hero1.health, 100);
+  });
+
+  it("should have a favourite food", function(){
+    assert.strictEqual(hero1.favouriteFood, "Rabbit");
   });
 
 })
