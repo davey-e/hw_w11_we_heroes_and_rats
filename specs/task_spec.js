31af3d84de74fad33e0a9d1ef709f83fce7fabfd
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-17 23:41:45 +0000

    Added Task with description property with test for description

---
+++ specs/task_spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const Task = require('../task.js');
+
+describe("Task Tests", function(){
+
+  beforeEach("Setup", function(){
+    task1 = new Task("Find the Holy Grail");
+  });
+
+  it("should have a description", function(){
+    assert.strictEqual(task1.description, "Find the Holy Grail");
+  })
+
+})
