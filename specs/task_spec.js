153cdcc736e65755a6a060e32044fc1b2edb72da
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-18 00:08:01 +0000

    Added difficulty property to Task with test

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -4,11 +4,15 @@ const Task = require('../task.js');
 describe("Task Tests", function(){
 
   beforeEach("Setup", function(){
-    task1 = new Task("Find the Holy Grail");
+    task1 = new Task("Find the Holy Grail", "Almost impossible");
   });
 
   it("should have a description", function(){
     assert.strictEqual(task1.description, "Find the Holy Grail");
-  })
+  });
+
+  it("should have a difficulty level", function(){
+    assert.strictEqual(task1.difficulty, "Almost impossible");
+  });
 
 })
