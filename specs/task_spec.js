40a5c5df40285f03cf65bcd5e9ddc463bc437b7f
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-18 23:10:17 +0000

    Added reward property to Task with test

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -4,7 +4,7 @@ const Task = require('../task.js');
 describe("Task Tests", function(){
 
   beforeEach("Setup", function(){
-    task1 = new Task("Find the Holy Grail", "Almost impossible", "Before King Arthur has a tantrum");
+    task1 = new Task("Find the Holy Grail", "Almost impossible", "Before King Arthur has a tantrum", 100);
   });
 
   it("should have a description", function(){
@@ -17,6 +17,10 @@ describe("Task Tests", function(){
 
   it("should have an urgency level", function(){
     assert.strictEqual(task1.urgency, "Before King Arthur has a tantrum");
+  });
+
+  it("should have a reward", function(){
+    assert.strictEqual(task1.reward, 100);
   });
 
 })
