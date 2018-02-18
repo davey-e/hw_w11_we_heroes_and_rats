31c0311fe594b937c874f7308831b32273a0e0ab
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-18 00:11:06 +0000

    Added urgency property to Task with test

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -4,7 +4,7 @@ const Task = require('../task.js');
 describe("Task Tests", function(){
 
   beforeEach("Setup", function(){
-    task1 = new Task("Find the Holy Grail", "Almost impossible");
+    task1 = new Task("Find the Holy Grail", "Almost impossible", "Before King Arthur has a tantrum");
   });
 
   it("should have a description", function(){
@@ -13,6 +13,10 @@ describe("Task Tests", function(){
 
   it("should have a difficulty level", function(){
     assert.strictEqual(task1.difficulty, "Almost impossible");
+  });
+
+  it("should have an urgency level", function(){
+    assert.strictEqual(task1.urgency, "Before King Arthur has a tantrum");
   });
 
 })
